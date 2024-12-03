71262712dea5361d29e647d6354272195dfabab4
LadyJaine <fullsucsess@gmail.com>
2024-12-03 23:32:10 +0500

    Add files via upload

---
+++ practice_1.py
@@ -0,0 +1,13 @@
+print('Task 1')
+print(9 ** 0.5 * 5)
+
+print('Task 2 - Логика')
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+print('Task 3 - Школьная загадка')
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+print('Task 4 - Первый после точки')
+print(int(float('123.456') * 10) % 10)
